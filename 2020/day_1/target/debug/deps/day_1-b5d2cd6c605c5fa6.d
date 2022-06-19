4e12abe571817c62b2fcb7ea3727932f95861d00
jammymalina <jakub.jantosik@gmail.com>
2022-06-19 18:54:20 +0200

    Removed target

--- 2020/day_1/target/debug/deps/day_1-b5d2cd6c605c5fa6.d
+++
@@ -1,6 +0,0 @@
-/home/jakub/Workspace/rust/advent_of_code/2020/day_1/target/debug/deps/day_1-b5d2cd6c605c5fa6.rmeta: src/main.rs src/input.txt
-
-/home/jakub/Workspace/rust/advent_of_code/2020/day_1/target/debug/deps/day_1-b5d2cd6c605c5fa6.d: src/main.rs src/input.txt
-
-src/main.rs:
-src/input.txt:
