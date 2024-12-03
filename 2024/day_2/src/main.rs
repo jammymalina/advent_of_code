a825b988027d3cff35499ffc3a8e9753848af762
jammymalina <jakub.jantosik@gmail.com>
2024-12-03 08:52:14 +0100

    Update

--- 2024/day_2/src/main.rs
+++ 2024/day_2/src/main.rs
@@ -1,27 +1,37 @@
-fn count_safe_reports(reports: Vec<Vec<i32>>) {
+fn count_safe_reports(reports: &[Vec<i32>]) {
     let safe_report_count: u32 = reports
         .iter()
         .map(|report| {
+            if report.len() <= 1 {
+                return 0;
+            }
+
             let differences: Vec<i32> = report.windows(2).map(|a| a[1] - a[0]).collect();
             let gradient_check = if differences[0] < 0 {
                 |x: i32| x < 0
             } else {
                 |x: i32| x > 0
             };
-            differences
-                .iter()
-                .all(|&x| gradient_check(x) && (1..4).contains(&x.abs())) as u32
+            u32::from(
+                differences
+                    .iter()
+                    .all(|&x| gradient_check(x) && (1..4).contains(&x.abs())),
+            )
         })
         .sum();
 
     println!("Number of safe reports: {safe_report_count}");
 }
 
-fn count_safe_reports_dampener(reports: Vec<Vec<i32>>) {
+fn count_safe_reports_dampener(reports: &[Vec<i32>]) {
     let safe_report_count: u32 = reports
         .iter()
         .map(|report| {
             let validate = |test_report: &Vec<i32>| {
+                if test_report.len() <= 1 {
+                    return false;
+                }
+
                 let differences: Vec<i32> = test_report.windows(2).map(|a| a[1] - a[0]).collect();
                 let gradient_check = if differences[0] < 0 {
                     |x: i32| x < 0
@@ -33,15 +43,17 @@ fn count_safe_reports_dampener(reports: Vec<Vec<i32>>) {
                     .all(|&x| gradient_check(x) && (1..4).contains(&x.abs()))
             };
 
-            (validate(report)
-                || (0..report.len()).any(|skip_index| {
-                    let test_report: Vec<i32> = report[..skip_index]
-                        .iter()
-                        .chain(report[skip_index + 1..].iter())
-                        .cloned()
-                        .collect();
-                    validate(&test_report)
-                })) as u32
+            u32::from(
+                validate(report)
+                    || (0..report.len()).any(|skip_index| {
+                        let test_report: Vec<i32> = report[..skip_index]
+                            .iter()
+                            .chain(report[skip_index + 1..].iter())
+                            .copied()
+                            .collect();
+                        validate(&test_report)
+                    }),
+            )
         })
         .sum();
 
@@ -55,9 +67,10 @@ fn main() {
     let reports: Vec<Vec<i32>> = lines
         .map(|line| {
             let parts: Vec<&str> = line.split_whitespace().collect();
-            if parts.len() < 2 {
-                panic!("Invalid input, expected more than 1 number");
-            }
+            assert!(
+                parts.len() >= 2,
+                "Invalid input, expected more than 1 number"
+            );
 
             parts
                 .into_iter()
@@ -66,6 +79,6 @@ fn main() {
         })
         .collect();
 
-    count_safe_reports(reports.clone());
-    count_safe_reports_dampener(reports.clone());
+    count_safe_reports(&reports);
+    count_safe_reports_dampener(&reports);
 }
