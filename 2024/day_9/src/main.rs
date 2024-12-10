15b22203e2e607721fd94793d822d3829a50d52f
jammymalina <jakub.jantosik@gmail.com>
2024-12-10 21:17:06 +0100

    Update

--- 2024/day_9/src/main.rs
+++ 2024/day_9/src/main.rs
@@ -1,118 +1,118 @@
+use std::cmp::Ordering;
+
+#[derive(Clone, Copy, Debug)]
+enum FileBlock {
+    File(u32, u32),
+    Empty(u32),
+}
+
 struct FileSystem {
-    disk_map: Vec<u32>,
+    blocks: Vec<FileBlock>,
 }
 
 impl FileSystem {
     fn new(disk_map: &str) -> Self {
-        Self {
-            disk_map: disk_map.chars().filter_map(|c| c.to_digit(10)).collect(),
+        let mut blocks = vec![];
+
+        let mut is_file = true;
+        let mut file_id = 0;
+        for c in disk_map.chars() {
+            let block_count = c.to_digit(10).unwrap();
+            if is_file {
+                assert!(block_count > 0, "File must have at least one block");
+                blocks.push(FileBlock::File(file_id, block_count));
+                file_id += 1;
+            } else {
+                blocks.push(FileBlock::Empty(block_count));
+            }
+
+            is_file = !is_file;
         }
+
+        Self { blocks }
     }
 
-    fn move_files_to_left(&mut self) -> usize {
-        let get_file_id = |map_index| map_index / 2;
-        let add_to_checksum = |acc, disk_position, file_id, block_count| {
-            let mut result = acc;
-            let mut iter = disk_position;
-            for _ in 0..block_count {
-                result += iter * file_id;
-                iter += 1;
+    fn compress_fragmented(&mut self) -> u64 {
+        let mut left_index = 0;
+        let mut right_index = self.blocks.len() - 1;
+
+        while left_index < right_index {
+            while left_index < right_index
+                && (matches!(self.blocks[left_index], FileBlock::File(_, _))
+                    || matches!(self.blocks[left_index], FileBlock::Empty(0)))
+            {
+                left_index += 1;
             }
-            (iter, result)
-        };
 
-        let mut checksum = 0;
+            while left_index < right_index
+                && matches!(self.blocks[right_index], FileBlock::Empty(_))
+            {
+                right_index -= 1;
+            }
 
-        let mut disk_position = 0;
-        let mut left_map_index = 0;
-        let mut right_map_index = if self.disk_map.len() % 2 == 0 {
-            self.disk_map.len() - 2
-        } else {
-            self.disk_map.len() - 1
-        };
-
-        let mut last_remaining = None;
-        while left_map_index < right_map_index {
-            let block_count = self.disk_map[left_map_index];
-            (disk_position, checksum) = add_to_checksum(
-                checksum,
-                disk_position,
-                get_file_id(left_map_index),
-                block_count,
-            );
-
-            left_map_index += 1;
-            if left_map_index >= right_map_index {
+            if left_index >= right_index {
                 break;
             }
 
-            let mut free_space = self.disk_map[left_map_index];
-            if let Some((block_count, map_index)) = last_remaining {
-                if free_space < block_count {
-                    last_remaining = Some((block_count - free_space, map_index));
-                    (disk_position, checksum) = add_to_checksum(
-                        checksum,
-                        disk_position,
-                        get_file_id(map_index),
-                        block_count - free_space,
-                    );
-                    self.disk_map[left_map_index] = 0;
-                    continue;
+            let empty_block_count = match self.blocks.get(left_index) {
+                Some(FileBlock::Empty(block_count)) => *block_count,
+                _ => unreachable!(),
+            };
+
+            let (file_id, file_block_count) = match self.blocks.get(right_index) {
+                Some(FileBlock::File(file_id, block_count)) => (*file_id, *block_count),
+                _ => unreachable!(),
+            };
+
+            match file_block_count.cmp(&empty_block_count) {
+                Ordering::Less | Ordering::Equal => {
+                    self.blocks[left_index] = FileBlock::File(file_id, file_block_count);
+                    self.blocks[right_index] = FileBlock::Empty(file_block_count);
+
+                    let free_space = empty_block_count - file_block_count;
+                    if free_space > 0 {
+                        self.blocks
+                            .insert(left_index + 1, FileBlock::Empty(free_space));
+                    }
                 }
+                Ordering::Greater => {
+                    self.blocks[left_index] = FileBlock::File(file_id, empty_block_count);
 
-                (disk_position, checksum) =
-                    add_to_checksum(checksum, disk_position, get_file_id(map_index), block_count);
-                last_remaining = None;
-                free_space -= block_count;
-                self.disk_map[left_map_index] = free_space;
+                    let leftover_file_block_count = file_block_count - empty_block_count;
 
-                right_map_index = map_index - 2;
-            }
-
-            while free_space > 0 && left_map_index < right_map_index {
-                let block_count = self.disk_map[right_map_index];
-                if block_count > free_space {
-                    last_remaining = Some((block_count - free_space, right_map_index));
-                    (disk_position, checksum) = add_to_checksum(
-                        checksum,
-                        disk_position,
-                        get_file_id(right_map_index),
-                        free_space,
-                    );
-                    self.disk_map[left_map_index] = 0;
-                    break;
+                    self.blocks[right_index] = FileBlock::File(file_id, leftover_file_block_count);
+                    self.blocks
+                        .insert(right_index + 1, FileBlock::Empty(empty_block_count));
                 }
-
-                (disk_position, checksum) = add_to_checksum(
-                    checksum,
-                    disk_position,
-                    get_file_id(right_map_index),
-                    block_count,
-                );
-                free_space -= block_count;
-                self.disk_map[left_map_index] = free_space;
-
-                right_map_index -= 2;
             }
-            left_map_index += 1;
         }
 
-        if let Some((block_count, map_index)) = last_remaining {
-            (disk_position, checksum) =
-                add_to_checksum(checksum, disk_position, get_file_id(map_index), block_count);
-            right_map_index = map_index - 2;
-        }
+        self.checksum()
+    }
 
-        for map_index in (left_map_index..right_map_index).step_by(2) {
-            (disk_position, checksum) = add_to_checksum(
-                checksum,
-                disk_position,
-                get_file_id(map_index),
-                self.disk_map[map_index],
-            );
-        }
+    fn compress_non_fragmented(&mut self) -> u64 {
+        self.checksum()
+    }
 
-        checksum
+    fn checksum(&self) -> u64 {
+        let mut position = 0;
+        self.blocks
+            .iter()
+            .map(|block| match block {
+                FileBlock::Empty(block_size) => {
+                    position += block_size;
+                    0
+                }
+                FileBlock::File(file_id, block_size) => {
+                    let mut result = 0;
+                    for _ in 0..*block_size {
+                        result += u64::from(position) * u64::from(*file_id);
+                        position += 1;
+                    }
+                    result
+                }
+            })
+            .sum()
     }
 }
 
@@ -120,6 +120,6 @@ fn main() {
     let input = include_str!("input.txt").trim();
 
     let mut fs = FileSystem::new(input);
-    let checksum = fs.move_files_to_left();
+    let checksum = fs.compress_fragmented();
     println!("Checksum after moving files to the left: {checksum}");
 }
